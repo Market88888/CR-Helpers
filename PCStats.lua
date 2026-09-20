810e41e1129d7609feaef2918b12555ea665cc59
Market88888 <arizonam30@gmail.com>
2026-09-20 12:13:23 +0300

    Update SCRIPT_VER to version 1.8.2

--- PCStats.lua
+++ PCStats.lua
@@ -13,7 +13,7 @@ script_author("Marco_Santiago")
 --  Сравнение с GitHub: manifest.json в репо Market88888/CR-Helpers
 --  Если там версия НОВЕЕ SCRIPT_VER → доступно обновление
 -- ============================================================
-local SCRIPT_VER = "1.8.0"
+local SCRIPT_VER = "1.8.2"
 script_version(SCRIPT_VER)
 
 -- интервал автопроверки обновлений (минуты). 1 или 5 — на выбор
